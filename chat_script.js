bce42a7fe787a862e0df32cf381c2b9263badcfc
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-06-13 09:24:09 +0300

    preparing client part

---
+++ chat_script.js
@@ -0,0 +1,9 @@
+// alert('form chat script')
+
+let username = document.getElementById(
+    'username');
+let chat_div  =document.getElementById('chat');
+let chat_msg = document.getElementById('chatmsg')
+let send_btn = document.getElementById('send')
+
+// use websocket to connection to the web socket server
